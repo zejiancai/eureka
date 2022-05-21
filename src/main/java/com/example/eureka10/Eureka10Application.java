6e10d8e86591f44a20a6786f5f79d040dda353c7
zejiancai <784663747@qq.com>
2022-05-21 21:20:17 +0800

    feat: first commit

---
+++ src/main/java/com/example/eureka10/Eureka10Application.java
@@ -0,0 +1,15 @@
+package com.example.eureka10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Eureka10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eureka10Application.class, args);
+    }
+
+}
